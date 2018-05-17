fa734fee8b1e94c1dfcc1fae213ccd35dfbbe113
Wojtek <wojga476@gmail.com>
2018-05-17 16:18:49 +0200

    Stworzenie pierwszego projektu

---
+++ src/Zadanie.java
@@ -0,0 +1,9 @@
+public class Zadanie {
+
+    public static void main(String[] args) {
+        System.out.println("Ucze");
+        System.out.println("sie");
+        System.out.println("programowania");
+
+    }
+}
